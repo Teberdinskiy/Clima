acc53ac9fa18d58957f7abf43576f31d3ccbac9f
Alikhan Batchaev <loko-han@mail.ru>
2020-02-01 20:27:46 +0300

    новый файл WeatherManager

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,13 +8,42 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
-
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self
     }
-
-
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != ""{
+            return true
+        } else {
+            textField.placeholder = "Введите что-нибудь"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+    }
+    
 }
 
---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by 🧔🏻Alikhan Batchaev on 01.02.2020.
+//  Copyright © 2020 Alikhan Batchaev. All rights reserved.
+//
+
+import UIKit
+
+struct WeatherManager {
+    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?appid=1407ee8c86647be611b8ec34aeb7e187&q=Stavropol&units=metric"
+}
