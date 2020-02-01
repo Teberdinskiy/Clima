39cbc5390a86ccdfe05e2146fe7cea7e85b91d48
Alikhan Batchaev <loko-han@mail.ru>
2020-02-01 21:14:43 +0300

    получение данных

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -9,5 +9,39 @@
 import UIKit
 
 struct WeatherManager {
-    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?appid=1407ee8c86647be611b8ec34aeb7e187&q=Stavropol&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=1407ee8c86647be611b8ec34aeb7e187&&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        
+        // Create a URL
+        if let url = URL(string: urlString) {
+            
+            // Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            // Give the session a task
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            
+            // Start the task
+            task.resume()
+            
+            
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+    }
 }
